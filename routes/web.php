2e1ba28e32d743c62903bfa34eb1c9fe0e98de94
Hafiz Caniago <hafizcode02@gmail.com>
2023-03-08 10:23:02 +0700

    deleting chirps

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('chirps', ChirpController::class)->only(['index', 'store', 'edit', 'update']);
+    Route::resource('chirps', ChirpController::class)->only(['index', 'store', 'edit', 'update', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
